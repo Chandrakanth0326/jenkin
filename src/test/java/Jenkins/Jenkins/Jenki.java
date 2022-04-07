a1110daf15bbcbf9338dc80d88ba2f0ca7a4757c
cvantettinavar@gmail.com <cvantettinavar@gmail.com>
2022-04-07 14:52:16 +0530

    1st commit

---
+++ src/test/java/Jenkins/Jenkins/Jenki.java
@@ -0,0 +1,14 @@
+package Jenkins.Jenkins;
+
+import org.testng.annotations.Test;
+
+public class Jenki 
+{
+	@Test
+	public void  Jenki()
+	{
+		System.out.println("bnbnc,n,zxmv");
+		System.out.println("jbdsvfj");
+	}
+
+}
